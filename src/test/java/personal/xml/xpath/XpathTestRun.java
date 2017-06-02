c954192b3e5b71a6e34be973eef71a6453c83f74
echeng <echeng@ci.com>
2017-06-02 10:14:53 -0400

    update xpath test case and note.txt

--- src/test/java/personal/xml/xpath/XpathTestRun.java
+++ src/test/java/personal/xml/xpath/XpathTestRun.java
@@ -1,7 +1,61 @@
 package personal.xml.xpath;
 
-public class XpathTestRun {
-    public static void main(String[] args){
+import org.springframework.core.io.ClassPathResource;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.*;
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class XpathTestRun {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException, TransformerException {
+
+        ClassPathResource resource = new ClassPathResource("personal/xml/xpath/xpath.xml");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(resource.getInputStream());
+
+
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        NodeList nodeList = (NodeList) xPath.evaluate("//text()[normalize-space()='']",
+                document,
+                XPathConstants.NODESET);
+
+        for (int i = 0; i < nodeList.getLength(); ++i) {
+            Node node = nodeList.item(i);
+            node.getParentNode().removeChild(node);
+        }
+
+
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        xPath = xPathFactory.newXPath();
+        XPathExpression xPathExpression = xPath.compile("//d1");
+
+        Object expressionResult = xPathExpression.evaluate(document, XPathConstants.NODE);
+
+        DOMSource domSource = new DOMSource((Node)expressionResult);
+
+        StringWriter stringWriter = new StringWriter();
+        StreamResult streamResult = new StreamResult(stringWriter);
+
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+        transformer.transform(domSource, streamResult);
+
+        System.out.println(stringWriter);
     }
 }
