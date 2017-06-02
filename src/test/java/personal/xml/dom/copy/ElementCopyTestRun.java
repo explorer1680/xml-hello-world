db4e783b90a3f59533aa852e7255e055247dca8d
echeng <echeng@ci.com>
2017-06-02 14:51:43 -0400

    add test case for copy.

---
+++ src/test/java/personal/xml/dom/copy/ElementCopyTestRun.java
@@ -0,0 +1,72 @@
+package personal.xml.dom.copy;
+
+import org.springframework.core.io.ClassPathResource;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.*;
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class ElementCopyTestRun {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException, TransformerException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document source = builder.parse(new ClassPathResource("personal/xml/dom/copy/copy.xml").getInputStream());
+
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xPath = xPathFactory.newXPath();
+        XPathExpression xPathExpression =  xPath.compile("//root/c");
+
+        Object copedNode = xPathExpression.evaluate(source,XPathConstants.NODE);
+
+        Document destination = builder.newDocument();
+        Element root2 = destination.createElement("root2");
+        destination.appendChild(root2);
+
+        Node newNode = ((Node)copedNode).cloneNode(true);
+        destination.adoptNode(newNode);
+        destination.getDocumentElement().appendChild(newNode);
+
+
+
+
+        xPath = XPathFactory.newInstance().newXPath();
+        NodeList nodeList = (NodeList) xPath.evaluate("//text()[normalize-space()='']",
+                destination,
+                XPathConstants.NODESET);
+
+        for (int i = 0; i < nodeList.getLength(); ++i) {
+            Node node = nodeList.item(i);
+            node.getParentNode().removeChild(node);
+        }
+
+
+        DOMSource domSource = new DOMSource(destination);
+
+
+
+        StringWriter stringWriter = new StringWriter();
+        StreamResult streamResult = new StreamResult(stringWriter);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+        transformer.transform(domSource, streamResult);
+
+        System.out.println(stringWriter);
+
+    }
+}
