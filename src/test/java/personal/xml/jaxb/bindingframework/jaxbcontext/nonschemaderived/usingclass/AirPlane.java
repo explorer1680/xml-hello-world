dc4db26cb2bd1eac21a2c40492823eec9e4ca6a6
Ethan Cheng <Ethan@jinsongs-air>
2017-06-11 15:33:24 -0400

    add test class for usingclass.

---
+++ src/test/java/personal/xml/jaxb/bindingframework/jaxbcontext/nonschemaderived/usingclass/AirPlane.java
@@ -0,0 +1,32 @@
+package personal.xml.jaxb.bindingframework.jaxbcontext.nonschemaderived.usingclass;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+//@XmlRootElement
+@XmlType
+@XmlAccessorType(XmlAccessType.FIELD)
+public class AirPlane {
+
+	private String planeModel;
+
+	public AirPlane() {
+		super();
+	}
+
+	public AirPlane(String planeModel) {
+		super();
+		this.planeModel = planeModel;
+	}
+
+	public String getPlaneModel() {
+		return planeModel;
+	}
+
+	public void setPlaneModel(String planeModel) {
+		this.planeModel = planeModel;
+	}
+
+}
---
+++ src/test/java/personal/xml/jaxb/bindingframework/jaxbcontext/nonschemaderived/usingclass/TestRun.java
@@ -0,0 +1,27 @@
+package personal.xml.jaxb.bindingframework.jaxbcontext.nonschemaderived.usingclass;
+
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+public class TestRun {
+	public static void main(String[] args) throws JAXBException{
+		JAXBContext context = JAXBContext.newInstance(Flight.class);
+		
+		Marshaller marshaller = context.createMarshaller();
+		
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		
+		StringWriter sw = new StringWriter();
+		
+		AirPlane airbus330 = new AirPlane("A330");
+		Flight ac032 = new Flight("AC032", airbus330);
+		
+		marshaller.marshal(ac032, sw);
+		
+		System.out.println(sw);
+	}
+
+}
