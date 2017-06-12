8498331b4cfa9fd1867a41bc5d3906349a339ea7
echeng <echeng@ci.com>
2017-06-12 17:00:42 -0400

    add new test case to unmarshaller.transformsource.

---
+++ src/test/java/personal/xml/jaxb/bindingframework/unmarshalling/transformsource/TransformSourceTestRun.java
@@ -0,0 +1,39 @@
+package personal.xml.jaxb.bindingframework.unmarshalling.transformsource;
+
+import org.springframework.core.io.ClassPathResource;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.dom.DOMSource;
+import java.io.IOException;
+
+public class TransformSourceTestRun {
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, JAXBException {
+        ClassPathResource xmlInstance = new ClassPathResource("personal/xml/jaxb/bindingframework/unmarshalling/transformsource/employee.xml");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(xmlInstance.getInputStream());
+        DOMSource domSource = new DOMSource(doc);
+
+        JAXBContext context = JAXBContext.newInstance(Employee.class);
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        Employee employee = (Employee)unmarshaller.unmarshal(domSource);
+
+        System.out.println(employee);
+
+    }
+
+
+
+
+}
