ed24e2d0d0cacd4a74969f2910d11350afcb81b4
echeng <echeng@ci.com>
2017-06-20 11:18:48 -0400

    add test case for binding.elementclass

--- src/test/java/personal/xml/jaxb/bindingframework/binding/elementclass/ElementClassTestRun.java
+++ src/test/java/personal/xml/jaxb/bindingframework/binding/elementclass/ElementClassTestRun.java
@@ -1,4 +1,34 @@
 package personal.xml.jaxb.bindingframework.binding.elementclass;
 
+import org.springframework.core.io.ClassPathResource;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
+
 public class ElementClassTestRun {
+    public static void main(String[] args) throws JAXBException, IOException {
+        JAXBContext context = JAXBContext.newInstance("personal.xml.jaxb.bindingframework.binding.elementclass");
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        Complex complex = (Complex)unmarshaller.unmarshal(new ClassPathResource("personal/xml/jaxb/bindingframework/binding/elementclass/elementclass.xml").getInputStream());
+
+        System.out.println(complex);
+
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,true);
+
+        AComplexType aComplexType = new AComplexType();
+        aComplexType.setASimpleElement(456878);
+
+
+        complex = new ObjectFactory().createComplex(aComplexType);
+
+        marshaller.marshal(complex, System.out);
+
+
+
+    }
 }
