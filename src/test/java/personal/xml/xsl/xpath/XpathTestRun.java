75c64047dd91c4188ce7ab9c21ffdb4c7f5943a8
Ethan Cheng <cjs1234567@hotmail.com>
2017-10-22 10:32:56 -0400

    move xpath to xsl.

--- src/test/java/personal/xml/xsl/xpath/XpathTestRun.java
+++ src/test/java/personal/xml/xsl/xpath/XpathTestRun.java
@@ -1,4 +1,4 @@
-package personal.xml.jaxp.xpath;
+package personal.xml.xsl.xpath;
 
 import org.springframework.core.io.ClassPathResource;
 import org.w3c.dom.Document;
@@ -19,7 +19,7 @@ import java.io.StringWriter;
 public class XpathTestRun {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException, TransformerException {
 
-        ClassPathResource resource = new ClassPathResource("personal/xml/jaxp/xpath/xpath.xml");
+        ClassPathResource resource = new ClassPathResource("personal/xml/xsl/xpath/xpath.xml");
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
