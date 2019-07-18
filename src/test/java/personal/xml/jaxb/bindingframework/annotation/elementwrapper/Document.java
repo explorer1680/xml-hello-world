884c4bd2e557b57800d0d85d8fb43afa3bf81829
echeng <echeng@ci.com>
2019-07-18 14:06:05 -0400

    add comments.

--- src/test/java/personal/xml/jaxb/bindingframework/annotation/elementwrapper/Document.java
+++ src/test/java/personal/xml/jaxb/bindingframework/annotation/elementwrapper/Document.java
@@ -2,8 +2,10 @@ package personal.xml.jaxb.bindingframework.annotation.elementwrapper;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name ="doc2")
 public class Document {
     private String name;
 
@@ -12,4 +14,6 @@ public class Document {
     public Document(String name) {
         this.name = name;
     }
+
+    public Document(){}
 }
